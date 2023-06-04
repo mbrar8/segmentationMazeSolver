0052bb8ae2a8433fbf785f6edc939a5c817ff60d
Maheep Brar <maheep.s.brar@gmail.com>
2023-06-03 22:20:48 -0700

    Edited training to include validation and early stopping

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import torch.optim as optim
 from torchvision.transforms import ToTensor
 from unet import UNet
 from torch.utils.data import DataLoader
+from torch.utils.data import random_split
 import os
 from maze_dataset import MazeDataset
 import matplotlib.pyplot as plt
@@ -20,33 +21,52 @@ lossfxn = nn.BCELoss()
 optimizer = optim.Adam(model.parameters(), lr=lr)
 
 
-train_dataset = MazeDataset(transform=ToTensor())
+maze_dataset = MazeDataset(transform=ToTensor())
+train_dataset, val_dataset = random_split(maze_dataset, [0.8, 0.2])
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
 
 
 losses = []
+min_val_loss = np.inf
+early_stopping = 0
+early_stopping_limit = 5
 print("Starting")
 for epoch in range(epochs):
-    rl = 0.0
+    tl = 0.0
+    model.train()
     for images, labels in train_loader:
+        optimizer.zero_grad()
         output = model(images)
-        print(labels.shape)
-        print(torch.unique(labels))
-        print(output.shape)
-        print(torch.unique(output))
 
         loss = lossfxn(torch.sigmoid(output), labels)
 
-        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        rl += loss.item()
+        tl += loss.item()
 
-    currloss = rl / len(train_loader)
-    losses.append(currloss)
+    currloss = tl / len(train_loader)
     print(f"Epoch [{epoch+1}/{epochs}], Loss: {currloss:.4f}")
 
+    vl = 0.0
+    model.eval()
+    for images, labels in val_loader:
+        output = model(images)
+        loss = lossfxn(torch.sigmoid(output), labels)
+        vl += loss.item()
+
+    vlloss = vl / len(val_loader)
+    losses.append((currloss, vlloss))
+    print(f"Epoch [{epoch+1}/{epochs}], Val Loss: {vlloss:.4f}")
+    if min_val_loss > vlloss:
+        min_val_loss = vlloss
+        early_stopping = 0
+    else:
+        early_stopping += 1
+        if early_stopping >= early_stopping_limit:
+            print(f"Early Stopping at Epoch {epoch+1}, Min VLoss: {min_val_loss:.4f}")
+            break
 
 torch.save(model.state_dict(), 'unet_maze.pth')
 
